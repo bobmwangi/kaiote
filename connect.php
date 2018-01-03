a1630f23d145b5c528140ec9518c516acb5a3ee9
Bob Mwangi <bkelvinmwa@gmail.com>
2018-01-03 15:19:26 +0300

    removed password

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
-$connection = mysql_connect('localhost', 'mamacare_bobapp', 'kenyatta47');
+$connection = mysql_connect('localhost', '*****', '*****');
 if (!$connection){
     die("Database Connection Failed" . mysql_error());
 }
-$select_db = mysql_select_db('mamacare_bob');
+$select_db = mysql_select_db('*******');
 if (!$select_db){
     die("Database Selection Failed" . mysql_error());
 }
